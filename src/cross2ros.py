646de24b2500b91c4e624b034ad811ba71b66be2
leca <100461457+hmoyen@users.noreply.github.com>
2024-08-05 17:09:32 -0300

    add ros integration

--- src/cross2ros.py
+++ src/cross2ros.py
@@ -10,19 +10,20 @@ from sensor_msgs.msg import Image
 
 from std_msgs.msg import String, Int16MultiArray
 
-import pyzbar.pyzbar as pyzbar
 
 import numpy as np
 
 
 
-class codeDetector:
+class crossDetector():
 
     def __init__(self):
+        self.publisher = rospy.Publisher("/sky_vision/cross_center", Int16MultiArray, queue_size=1)
 
-        print("Created publisher")
-
+        self.image_publisher = rospy.Publisher("/sky_vision/cross", Image, queue_size=1)
+        print("Created publishers")
         self.bridge = CvBridge()
+        self.point = Int16MultiArray()
 
         self.kernel = np.ones((5, 5), np.uint8)
         self.min_area = 6000
@@ -32,50 +33,67 @@ class codeDetector:
         # PERDAO PELO CÓDIGO NOJENTO PRECISO DORMIR
         og = image
 
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        hsv_frame = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         
-        #escolha entre threshold dinamico ou n
+
+        lower_red1 = np.array([149, 115, 50])
+        upper_red1 = np.array([255, 255, 255])
+        
+        # Create masks for red color
+        image = cv2.inRange(hsv_frame, lower_red1, upper_red1)
+        
+        # Choose between dynamic threshold or not
         #image = cv2.medianBlur(image, 3)
         #_, threshold = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY_INV)
         
         threshold = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 15, 7)
 
-        contours, _ = cv2.findContours(threshold, cv2.RETR_TREE ,cv2.CHAIN_APPROX_SIMPLE)
+        contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        #cv2.drawContours(og, contours, -1, (0, 0 ,255), 3)
         for contour in contours:
-
             if cv2.contourArea(contour) > self.min_area:
-                
-                epsilon = 0.005*cv2.arcLength(contour,True)
+
+                epsilon = 0.005 * cv2.arcLength(contour, True)
                 contour = cv2.approxPolyDP(contour, epsilon, True)
                 
                 hull = cv2.convexHull(contour, returnPoints=False)
                 defects = cv2.convexityDefects(contour, hull)
                 
-                #AQUI A "LEN" MUDARIA DEPENDENDO DO H OU DO +
+                # Check if defects exist and are four
                 if type(defects) != type(None) and len(defects) == 4:
+                    # Draw the contour and calculate the center
+                    M = cv2.moments(contour)
+                    if M["m00"] != 0:
+                        cX = int(M["m10"] / M["m00"])
+                        cY = int(M["m01"] / M["m00"])
+                        self.point.data = (cX, cY)
+                        self.publisher.publish(self.point)
 
+                    else:
+                        cX, cY = 0, 0
 
-                    #AQUI VIRIA UM FILTRO DE COR INSANO
-                                            
+                    cv2.circle(image, (cX, cY), 7, (255, 0, 0), -1)  # Draw center
+                    cv2.putText(image, "center", (cX - 20, cY - 20), 
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+                    
+                    cv2.drawContours(image, [contour], -1, (0, 255, 0), 3)
+                    
                     for i in range(defects.shape[0]):
-                        s,e,f,d = defects[i,0]
+                        s, e, f, d = defects[i, 0]
                         start = tuple(contour[s][0])
                         end = tuple(contour[e][0])
                         far = tuple(contour[f][0])
-                        #cv2.line(og,start,end,[0,255,0],2)
-                        cv2.circle(og,far,10,[0,0,255],-1)    
-
-                # print(defects)
-                if type(defects) != type(None) and len(defects) == 4:
-                    cv2.drawContours(og, [contour], 0, (0,255,0), 3)    
+                        cv2.circle(image, far, 10, [0, 0, 255], -1)
                 else:
-                    cv2.drawContours(og, [contour], 0, (0,0,255), 3)
+                    cv2.drawContours(image, [contour], -1, (0, 0, 255), 3)
 
-        cv2.imshow("AAA", og)
-        cv2.imshow("TWA", threshold2)
-        cv2.waitKey(1)
+                # Convert the image with overlay to ROS message and publish
+        image = self.bridge.cv2_to_imgmsg(og, "bgr8")
+        self.image_publisher.publish(image)
+        # cv2.imshow("AAA", og)
+        # cv2.imshow("TWA", threshold)
+        # cv2.waitKey(1)
 
 
     def callback(self, message):
@@ -88,12 +106,28 @@ class codeDetector:
 
 def main():
 
-    rospy.init_node("code_detector")
+    rospy.init_node("cross_detector")
 
-    detector = codeDetector()
+    detector = crossDetector()
 
-    rospy.Subscriber("/sky_vision/camera_0/image_raw", Image,detector.callback)
+    rate = rospy.Rate(10)  # Adjust the rate as needed
+    subscribed = False
 
+    while not rospy.is_shutdown():
+    
+        if not subscribed:
+            topics = rospy.get_published_topics()
+            topic_list = [topic for topic, _ in topics]
+            if "/sky_vision/down_cam/img_raw" in topic_list:
+                rospy.Subscriber("/sky_vision/down_cam/img_raw", Image, detector.callback)
+                subscribed = True
+                print("Subscribed to /sky_vision/down_cam/img_raw")
+            elif "/sky_vision/generic_cam/img_raw" in topic_list:
+                rospy.Subscriber("/sky_vision/generic_cam/img_raw", Image, detector.callback)
+                subscribed = True
+                print("Subscribed to /sky_vision/generic_cam/img_raw")
+        rate.sleep()
+    
     rospy.spin()
 
 
